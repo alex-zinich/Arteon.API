d16d2f903e06b2bbad6da33d5223897d169dfb8e
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-02-15 13:04:15 +0200

    Change IHotelContext

--- Arteon.Core/Services/Database/IHotelContext.cs
+++ Arteon.Core/Services/Database/IHotelContext.cs
@@ -6,6 +6,10 @@ namespace Arteon.Core.Services.Database
     public interface IHotelContext
     {
         DbSet<Room> Rooms { get; set; }
+        DbSet<Client> Clients { get; set; }
+        DbSet<Booking> Bookings { get; set; }
+        DbSet<Service> Services { get; set; }
+        DbSet<BookingService> BookingServices { get; set; }
         int SaveChanges();
     }
 }
